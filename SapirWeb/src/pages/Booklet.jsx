031f1b312c39b7ee4ded7ea3f62fbc685fcdbf9d
Itay Meshulam <itay@adom.technology>
2025-06-29 15:05:54 +0300

    wip

--- SapirWeb/src/pages/Booklet.jsx
+++ SapirWeb/src/pages/Booklet.jsx
@@ -68,7 +68,7 @@ function Booklet() {
     try {
       setLoading(true);
       const response = await axios.post(
-        `${window.location.protocol}//${window.location.hostname}:7004/api/email/send-email`,
+        `${window.location.protocol}//${window.location.hostname}/api/email/send-email`,
         {
           name: form.name,
           email: form.email,
