658d3d12e6b5506fbd5ac4d354274ada8cc4e93e
Itay Meshulam <itay@adom.technology>
2025-06-29 09:22:45 +0300

    wip

--- SapirWeb/src/pages/Booklet.jsx
+++ SapirWeb/src/pages/Booklet.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./Booklet.css";
 import axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Booklet() {
   const [form, setForm] = useState({
@@ -12,8 +12,10 @@ function Booklet() {
   });
 
   const [errors, setErrors] = useState({});
+  const [serverError, setServerError] = useState(false);
   const [agree, setAgree] = useState(false);
-
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -63,98 +65,123 @@ function Booklet() {
       return;
     }
 
-   
     try {
-      const response = await axios.post(`${window.location.protocol}//${window.location.hostname}:7004/api/email/send-email`, {
-        name: form.name,
-        email: form.email,
-        phone: form.phone,
-      });
+      setLoading(true);
+      const response = await axios.post(
+        `${window.location.protocol}//${window.location.hostname}:7004/api/email/send-email`,
+        {
+          name: form.name,
+          email: form.email,
+          phone: form.phone,
+        }
+      );
 
       if (response.data.success) {
-        toast.success("החוברת נשלחה למייל שלך בהצלחה!");
+      
         setForm({ name: "", email: "", phone: "" });
         setAgree(false);
-      
       } else {
-        toast.error("אירעה שגיאה בשליחת המייל. נסי שוב.");
+        setServerError(true);
       }
-      
-     } catch (err) {
-       console.error("send email error:", err);
-       toast.error("אירעה שגיאה בשליחת המייל. נסי שוב.");
-      }
+    } catch (err) {
+      setServerError(true);
+      console.error("send email error:", err);
+    }
+    finally{
+      setShowModal(true);
+      setLoading(false);
+    }
   };
 
   return (
-    <div>
-    <img src="/text.jpeg" alt="Booklet" className="form-image" />
-    <p>
-      מה תקבלי בה?
-    <br />
-✔️משחק חוויתי לכל המשפחה שדרכו ניתן לסכם את שנת הלימודים שהסתיימה - הכולל הוראות, לוח משחק, שחקנים, קובייה, קלפי שאלה וקלפי פקודה.
-<br />
-✔️לוח חופש לתכנון החופש הגדול
-<br />
-✔️רעיונות לפעיליות ומקומות בילוי. 
-<br />
-✔️כרטיסיות למילוי לוח החופש ביחד.
-<br />
-<br />
-</p>
-<h2>החוברת להורדה ללא עלות!</h2>
-    <div className="form-container">
-      <div className="form-header">
-        מלאי את הפרטים והחוברת תשלח אלייך ישירות למייל
+    <div className="container">
+      <img src="/text.jpeg" alt="Booklet" className="form-image" />
+
+      <div className="bullets">
+        <div className="bullets-header">מה תקבלי בה?</div>
+        <div className="bullet">
+          ✔️משחק חוויתי לכל המשפחה שדרכו ניתן לסכם את שנת הלימודים שהסתיימה -
+          הכולל הוראות, לוח משחק, שחקנים, קובייה, קלפי שאלה וקלפי פקודה.
+        </div>
+        <div className="bullet">✔️לוח חופש לתכנון החופש הגדול.</div>
+        <div className="bullet">✔️רעיונות לפעיליות ומקומות בילוי.</div>
+        <div className="bullet">✔️כרטיסיות למילוי לוח החופש ביחד.</div>
       </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="שם מלא"
-          type="text"
-          name="name"
-          value={form.name}
-          onChange={handleChange}
-        />
-        {errors.name && <div className="error">{errors.name}</div>}
+      <h2 style={{ margin: 'auto', marginTop: '30px' }}>
+          החוברת להורדה ללא עלות!</h2>
+      <div className="form-container">
+        <div className="form-header">
+          מלאי את הפרטים והחוברת תשלח אלייך ישירות למייל
+        </div>
 
-        <input
-          placeholder="אימייל"
-          type="email"
-          name="email"
-          value={form.email}
-          onChange={handleChange}
-        />
-        {errors.email && <div className="error">{errors.email}</div>}
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="שם"
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+          />
+          {errors.name && <div className="error">{errors.name}</div>}
 
-        <input
-          dir="rtl"
-          placeholder="טלפון"
-          type="tel"
-          name="phone"
-          value={form.phone}
-          onChange={handleChange}
-        />
-        {errors.phone && <div className="error">{errors.phone}</div>}
+          <input
+            placeholder="אימייל"
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+          />
+          {errors.email && <div className="error">{errors.email}</div>}
 
-        <div className="checkbox-container">
-  <label>
-    <input
-      type="checkbox"
-      checked={agree}
-      onChange={(e) => {
-        setAgree(e.target.checked);
-        setErrors({ ...errors, agree: "" });
-      }}
-    />
-    אני מאשרת קבלת מיילים ועדכונים
-  </label>
-  {errors.agree && <div className="error">{errors.agree}</div>}
-</div>
+          <input
+            dir="rtl"
+            placeholder="טלפון"
+            type="tel"
+            name="phone"
+            value={form.phone}
+            onChange={handleChange}
+          />
+          {errors.phone && <div className="error">{errors.phone}</div>}
 
-        <button type="submit">שלחי לי את החוברת :)</button>
-      </form>
-    </div>
+          <div className="checkbox-container">
+            <label>
+              <input
+                type="checkbox"
+                checked={agree}
+                onChange={(e) => {
+                  setAgree(e.target.checked);
+                  setErrors({ ...errors, agree: "" });
+                }}
+              />
+              אני מאשרת קבלת מיילים ועדכונים
+            </label>
+            {errors.agree && <div className="error">{errors.agree}</div>}
+          </div>
+
+          <button type="submit">שלחי לי את החוברת :)</button>
+        </form>
+      </div>
+      {/* 🔥 Loader */}
+      {loading && (
+        <div className="loader-overlay">
+          <div className="spinner"></div>
+        </div>
+      )}
+
+      {/* 🎯 Modal */}
+      {showModal && (
+        <div className="modal-overlay">
+        {/* <div className="modal-overlay"> */}
+          <div className="modal-content">
+            {serverError ?  <h2>אירע שגיאה אנא נסו בשנית</h2> : 
+             <h2>החוברת נשלחה למייל שלך 🎉</h2>}
+            <button onClick={() => setShowModal(false)}>סגור</button>
+          </div>
+        </div>   
+      // </div>
+
+      )}
     </div>
   );
 }
